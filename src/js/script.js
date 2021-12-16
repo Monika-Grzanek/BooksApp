3a20bb2d2f3d1643ab5adcf394e7baf8919e461b
Monika Grzanek <monski@o2.pl>
2021-12-16 13:42:22 +0100

    Add initActions in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,11 @@ const select = {
   },
   listOf: {
     list: '.books-list',
-  }
+  },
+  image: {
+    coverImage: '.book__image',
+    favoriteCoverImage: '.favorite',
+  },
 };
 
 const templates = {
@@ -14,7 +18,8 @@ const templates = {
 };
     
 const listBook = document.querySelector(select.listOf.list);
-
+//const coverBooks = listBook.querySelectorAll(select.image.coverImage);
+//const favoriteCoverBook = select.image.favoriteCoverImage;
 
 function render(){
   for(let item of dataSource.books){
@@ -26,3 +31,27 @@ function render(){
 }
 
 render();
+
+function initActions(){
+  const coverBooks = listBook.querySelectorAll(select.image.coverImage);
+  const favoriteCoverBook = select.image.favoriteCoverImage;
+
+  const favoriteBooks = [];
+  for(let cover of coverBooks){
+    const coverDataId = cover.getAttribute('data-id');
+    cover.addEventListener('dblclick', function(event){
+      event.preventDefault();
+      const bookIndex = favoriteBooks.indexOf(coverDataId);
+      if(!favoriteBooks[bookIndex]){
+        favoriteBooks.push(coverDataId);
+        cover.classList.add(favoriteCoverBook);
+      } else {
+        favoriteBooks.splice(bookIndex, 1);
+        cover.classList.remove(favoriteCoverBook);
+      }
+    });
+  }
+  console.log('show favoriteBooks', favoriteBooks);
+}
+
+initActions();
